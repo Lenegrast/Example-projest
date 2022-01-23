20bb568ffd5e27fd0e614cc6472f1c4c2b1c9069
Lenegrast <valinurov.ilmir@inbox.ru>
2022-01-23 19:03:27 +0500

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 "use strict";
 
-const result = confirm("Are you here?");
-console.log(result);
+let numverOfFilms = prompt("Сколько фильмов вы уже посмотрели?", );
+
+let personalMovieDB = {
+    count: numverOfFilms,
+    movies: {},
+    actorns: {},
+    genres: [],
+    print: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Сколько фильмов вы уже посмотрели?", ),
+      d = prompt("На сколько оцените его?", "");
+      
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
